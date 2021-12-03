5d5ff9a6df51986399f665610a10b4344d90baf0
Alvin Wilbert <awilbert@student.ciputra.ac.id>
2021-12-03 11:27:55 +0800

    Code bilangan prima

--- BilanganPrima/Program.cs
+++ BilanganPrima/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan angka : ");
+        int input = Convert.ToInt32(Console.ReadLine());
+        int cek;
+        for (int i = 2; i <= input; i++)
+        {
+            cek = input % i;
+            if (cek == 0)
+            {
+                if (input == i)
+                    Console.WriteLine("Bilangan Prima");
+                else
+                {
+                    Console.WriteLine("bukan bilangan prima");
+                    break;
+                }
+            }
+        }
+    }
+}
